dcd7b4ae6bd4e6e088d875bc29308a84e7b8433f
Vadim Gagin <vadimgagin@yandex.ru>
2024-09-27 20:33:07 +0300

    Убрал лишнее в скрипте очистки

--- Скрипт для очистки.cmd
+++ Скрипт для очистки.cmd
@@ -1,36 +1,3 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "f4a8bb85-e675-40bf-8c8a-fbad308c794b",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.12.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
 git reset --hard
 git clean -f -d
 pause>nul
